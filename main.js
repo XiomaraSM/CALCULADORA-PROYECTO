458fcf97d18eb77ed0cfa52c0d78f75e7a202d24
XiomaraSM <sanchezmorenoxiomara@gmail.com>
2022-11-25 09:45:24 -0500

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log ("hola")
+
+var numpantalla "0";
+var pantallaconnumero="si";
+var usarpunto="no";
+var numespera=0;
+var operapendiente="";
+var solucion="";
+
+function ingresarnumero (x)
+{
+    if(x!== ".")
+    {
+        if (numpantalla=="0" || pantallaconnumero=="si")
+    }
+    else if (x!="")
+    {     
+       document.Calculadora.txtboxnros.value=X;
+    }
+}
